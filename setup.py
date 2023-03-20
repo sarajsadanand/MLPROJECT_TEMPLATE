ccb02500eecdbe999425c596017ac4d0f6ec2a96
Saraj Sadanand <sadanandsaraj@gmail.com>
2023-03-20 09:53:25 +0530

    setup.py and requirements.txt created

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    """
+    This function will return the list of requirements
+    """
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readline()
+        requirements=[req.replace("\n","") for req in requirements]
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Saraj',
+    author_email='sadanandsaraj@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt") # requires a list
+)
